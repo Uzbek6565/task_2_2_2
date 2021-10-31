05dac8818f3185f3176afd0e6972cd12e1904794
uzbek <tba-93@yandex.uz>
2021-10-31 22:08:24 +0500

    Preparing

--- src/main/java/com/example/task222/entity/Currency.java
+++ src/main/java/com/example/task222/entity/Currency.java
@@ -4,19 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Basket {
+public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @OneToMany
-    List<Product> productList;
+    private String name;
 
+    private boolean active = false;
 }
--- src/main/java/com/example/task222/entity/Product.java
+++ src/main/java/com/example/task222/entity/Product.java
@@ -1,4 +1,27 @@
 package com.example.task222.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(optional = false)
+    private Category category;
+
+    @OneToOne
+    private Attachment attachment;
+
+    private String code;
+
+
 }
